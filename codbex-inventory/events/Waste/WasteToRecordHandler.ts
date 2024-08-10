557af937c0b5072820957adb2f6e01f2a24a7ba6
bob4o-afk <bobkatasad@abv.bg>
2024-08-10 18:06:02 +0000

    fix: fixing events

--- codbex-inventory/events/Waste/WasteToRecordHandler.ts
+++ codbex-inventory/events/Waste/WasteToRecordHandler.ts
@@ -1,22 +1,17 @@
-import { StockAdjustmentRepository } from "../../gen/codbex-inventory/dao/StockAdjustments/StockAdjustmentRepository";
+import { WasteRepository } from "../../gen/codbex-inventory/dao/Waste/WasteRepository";
 import { CatalogueRepository } from "codbex-products/gen/codbex-products/dao/Catalogues/CatalogueRepository";
 
 export const trigger = (event) => {
-    const StockAdjustmentDao = new StockAdjustmentRepository();
+    const WasteDao = new WasteRepository();
     const CatalogueDao = new CatalogueRepository();
     const item = event.entity;
     const operation = event.operation;
-    const header = StockAdjustmentDao.findById(item.StockAdjustment);
-
-    if (!header || header.Store === undefined) {
-        throw new Error("Store is undefined in StockAdjustment header");
-    }
 
     if (operation === "create") {
         const catalogueRecords = CatalogueDao.findAll({
             $filter: {
                 equals: {
-                    Store: header.Store,
+                    Store: item.Store,
                     Product: item.Product,
                 },
             },
@@ -27,14 +22,22 @@ export const trigger = (event) => {
             catalogueRecord.Quantity = (catalogueRecord.Quantity - item.Quantity) < 0 ? 0 : (catalogueRecord.Quantity - item.Quantity);
             CatalogueDao.update(catalogueRecord);
         } else {
-            //no product has been found
-            console.log("No such product");
+            //No item has been found
+            // const newCatalogueRecord = {
+            //     Store: item.Store,
+            //     Product: item.Product,
+            //     Quantity: item.Quantity,
+            // };
+            // CatalogueDao.create(newCatalogueRecord);
         }
 
     } else if (operation === "update") {
-        // TODO: Implement update logic
+        // TODO: Implement update logic 
+        // the idea is to be able to get the enity before the update to get the Quantity and then to make
+        // an equation that looks like that:  catalogueRecord.Quantity -= (oldQuanity - item.Quantity)
     } else if (operation === "delete") {
         // TODO: Implement delete logic
+        // the same with update but like this: catalogueRecord.Quantity += oldQuanity
     } else {
         throw new Error("Unknown operation: " + operation);
     }
